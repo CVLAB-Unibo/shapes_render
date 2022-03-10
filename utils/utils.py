55281ecd0401d14b4e56453d71a24b75a11a484d
rspezialetti <riccardo.spezialetti@gmail.com>
2022-03-10 10:34:51 +0100

    feat: start to code

--- utils/utils.py
+++ utils/utils.py
@@ -1,5 +1,15 @@
 """
-Module utils
+Module containing generic utils.
+
 Author: Riccardo Spezialetti
 Mail: riccardo.spezialetti@unibo.it
 """
+import bpy  # type: ignore
+
+
+def remove_objects() -> None:
+    """
+    Remove all the objects in the scene.
+    """
+    for item in bpy.data.objects:
+        bpy.data.objects.remove(item)
